cf76e2750a0715df228ea5f78e09c766f0be436d
Alex Feoktistov <Feoktistovao@gmail.com>
2020-03-09 18:34:55 +0300

    Les.21 css dialog page
    add css for dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import components from './Dialogs.module.css';
 
 const Dialogs = ()=> {
     return (
-        <div>
-            Dialogs
+        <div className={components.dialogs}>
+            <div className={components.dialogs__items}>
+                <div className={components.dialog + ' ' + components.active}>
+                    Name1
+                </div>
+                <div className={components.dialog}>
+                    Name2
+                </div>
+                <div className={components.dialog}>
+                    Name3
+                </div>
+                <div className={components.dialog}>
+                    Name4
+                </div>
+            </div>
+            <div className={components.messages}>
+                <div className={components.message}>
+                    Hi
+                </div>
+                <div className={components.message}>
+                    Byt
+                </div>
+                <div className={components.message}>
+                    Buy
+                </div>
+            </div>
         </div>
     );
 }
