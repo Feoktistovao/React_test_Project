9b54d36e5c12d58820be958320026e1f4cae8485
Alex Feoktistov <Feoktistovao@gmail.com>
2020-03-09 17:57:10 +0300

    Les. 19
    Lesson 19. Add React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import logoSvg from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navigation";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
+        <BrowserRouter>
+            <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
-                <Profile/>
-        </div>
-
+                <div className='app-wrapper-content'>
+                    <Route component={Dialogs} path={"/dialogs"}/>
+                    <Route component={Profile} path={'/profile'}/>
+                    <Route component={News} path={'/news'}/>
+                    <Route component={Music} path={'/music'} />
+                    <Route component={Settings} path={'/settings'} />
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
