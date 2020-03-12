4782bbd288c072b2aaf71448729e178df476464a
Alex Feoktistov <Feoktistovao@gmail.com>
2020-03-12 20:33:23 +0300

    Les.35 Refactor
    -rerender.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () =>{
+
+}
 
 let state={
     profilePage : {
@@ -24,7 +26,7 @@ let state={
 
 }
 
-export let addPost = (postMessage) => {
+export const addPost = () => {
         let newPost= {
         id: 5,
         message: state.profilePage.newPostText,
@@ -35,9 +37,13 @@ export let addPost = (postMessage) => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText=newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe= (observer) =>{
+    rerenderEntireTree = observer;
 }
 
 export default state;
